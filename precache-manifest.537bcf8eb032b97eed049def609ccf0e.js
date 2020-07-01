84ec5455d2556b296a135336f3e645413936e454
Sajin Soman <Sajin.Soman@capita.co.uk>
2020-07-01 22:14:23 +0530

    Updates

---
+++ precache-manifest.537bcf8eb032b97eed049def609ccf0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73833e57a9de140ded0279b92a0a6687",
+    "url": "/FoxTodoApp/index.html"
+  },
+  {
+    "revision": "b7325830c0805bd54c87",
+    "url": "/FoxTodoApp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7d4eaef6705e8bfb815c",
+    "url": "/FoxTodoApp/static/js/2.d38aa328.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/FoxTodoApp/static/js/2.d38aa328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7325830c0805bd54c87",
+    "url": "/FoxTodoApp/static/js/main.0070bbf4.chunk.js"
+  },
+  {
+    "revision": "ecef325bc1748007d0cf",
+    "url": "/FoxTodoApp/static/js/runtime-main.088fa12e.js"
+  }
+]);
