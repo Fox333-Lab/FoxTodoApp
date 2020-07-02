6b05e0459af22d75a4663278b99bff6fd0c59a62
Sajin <67701997+Fox333-Lab@users.noreply.github.com>
2020-07-02 09:52:19 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyARYMfkZMaBzKxUf0-PUvTXkFwKYrkX8ro",
-  authDomain: "react-firebase-practice-31c7c.firebaseapp.com",
-  databaseURL: "https://react-firebase-practice-31c7c.firebaseio.com",
-  projectId: "react-firebase-practice-31c7c",
-  storageBucket: "react-firebase-practice-31c7c.appspot.com",
-  messagingSenderId: "230462173025",
-  appId: "1:230462173025:web:691d3ebb6afaf7a3091f20",
-  measurementId: "G-126MXFTN4W",
+//  add you firebase configurations here
 };
 
 var fire = firebase.initializeApp(firebaseConfig);
